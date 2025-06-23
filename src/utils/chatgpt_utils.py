7e431b48af83fca538b9dbf781ba49ad5913045d
cotulars <anton.my.akk@gmail.com>
2025-06-22 17:22:58 -0700

    hotfix

--- src/utils/chatgpt_utils.py
+++ src/utils/chatgpt_utils.py
@@ -27,7 +27,7 @@ def get_message_dict(msg, current_msg_map):
 
     if msg.is_forwarded:
         msg_type = 'FORWARDED_MESSAGE'
-        fwd_suffix = f' forwarded from {msg.forward_from.name}'
+        fwd_suffix = f' forwarded from {msg.forward_from}'
     elif msg.is_sticker:
         msg_type = 'STICKER'
     elif msg.media_content_type:
